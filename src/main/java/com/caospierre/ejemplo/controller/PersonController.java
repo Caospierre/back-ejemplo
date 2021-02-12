30f272e2b6986e1d910a2ed9aeeec647a88f1bd6
unknown <jppineda1@espe.edu.ec>
2021-02-12 17:58:10 -0500

    @Caospierre CORS FIX

--- src/main/java/com/caospierre/ejemplo/controller/PersonController.java
+++ src/main/java/com/caospierre/ejemplo/controller/PersonController.java
@@ -13,14 +13,8 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
+//Controller 
 @RestController
 @RequestMapping ("/api/person")
 public class PersonController {
@@ -28,7 +22,9 @@ public class PersonController {
     @Autowired
     private PersonService personaService;
 
+    @CrossOrigin
     @PostMapping("/create")
+
     private ResponseEntity<Person> save (@RequestBody Person person){
 
         Person temporal = personaService.create(person);
@@ -57,6 +53,7 @@ public class PersonController {
     private ResponseEntity<Optional<Person>> listPersonById (@PathVariable ("id") Long id){
         return ResponseEntity.ok(personaService.findById(id));
     }
+    @CrossOrigin
     @PostMapping("/login")
     public ResponseEntity<LoginPerson> loginUser(@RequestBody Person person) {
         String username = (String) person.getUsername();
